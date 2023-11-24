4c562e57bfbbf5906195a4233b339c60a32987ba
Aditya <pandeyaditya032002@gmail.com>
2023-11-24 19:46:09 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app=express()
+const app = express();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to mongo");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000")
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
