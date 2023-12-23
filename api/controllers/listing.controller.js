d5468d63d0b8ebab3b30273fb419e52d60c7f053
Aditya <pandeyaditya032002@gmail.com>
2023-12-23 21:34:56 +0530

    complete create listing page functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -2,6 +2,7 @@ import Listing from "../model/listing.model.js";
 
 export const createListing = async (req, res, next) => {
   try {
+    // console.log(req.body);
     const listing = await Listing.create(req.body);
     res.status(201).json(listing);
   } catch (error) {
