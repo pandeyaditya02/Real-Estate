5927dbf2ce692fe9c9bfa3c7ca657fb8e4f0f27f
Aditya <pandeyaditya032002@gmail.com>
2024-01-03 00:51:15 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      About
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About DTU Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        DTU Estate is a dedicated platform designed with the sole purpose of
+        addressing the accommodation challenges faced by students at our
+        esteemed college. Recognizing the struggle of students arriving from
+        various cities, DTU Estate serves as a vital solution, streamlining the
+        process of finding PGs, flats for rent, and properties for sale in close
+        proximity to the college.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Our platform empowers users to effortlessly create real estate listings,
+        fostering a community-driven approach to accommodation. Students can
+        showcase their available spaces for rent or sale, providing detailed
+        information on amenities, such as parking facilities, furnished or
+        unfurnished options, and more. Through the seamless integration of
+        user-generated listings and advanced search functionalities, DTU Estate
+        facilitates a user-friendly experience, ensuring that students can
+        quickly locate the ideal living spaces that meet their unique
+        preferences and requirements.
+      </p>
+      <p className="mb-4 text-slate-700">
+        With a focus on simplicity and efficiency, DTU Estate is committed to
+        easing the transition for students seeking accommodation near the
+        college. By harnessing the power of technology, we strive to create a
+        supportive environment that not only simplifies the housing search but
+        also contributes to fostering a sense of community among our diverse
+        student population. Welcome to DTU Estate, where finding your ideal
+        living space is just a click away.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
